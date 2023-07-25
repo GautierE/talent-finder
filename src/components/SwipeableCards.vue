4bad55f6db0005b4dc6ae2fb4d7b3522f225c78c
Gautier <gautier.edel.info@gmail.com>
2023-07-25 22:35:41 +0200

    fix: test fix avatar not loading on prod

--- src/components/SwipeableCards.vue
+++ src/components/SwipeableCards.vue
@@ -8,7 +8,7 @@
                 @mousemove="onMouseMove($event)" @mouseup="onMouseUp"
                 :style="{ transform: activeIndex === index ? `translateX(${currentX}px) rotate(${currentRotation}deg)` : `translateX(0px) rotate(0deg)` }">
                 <div class="flex flex-col items-center h-full px-4 pb-4 justify-evenly">
-                    <img :src="`public/avatars/avatar${candidate.id % 6}.svg`" alt="Avatar" class="w-[80%] md:w-[40%]" />
+                    <img :src="`public/avatars/avatar0.svg`" alt="Avatar" class="w-[80%] md:w-[40%]" />
                     <h3 class="mt-2 font-bold text-[22px] md:text-[35px] lg:text-[30px]">{{ candidate.firstName }} {{
                         candidate.lastName }}
                     </h3>
